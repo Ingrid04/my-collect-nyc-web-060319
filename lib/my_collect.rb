b8722b04b13adb5c134a265041596177120e6659
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-06-01 05:33:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collection(languages)
+  languages = ['ruby', 'javascript', 'python', 'objective-c']
+  languages.collect {|lang| lang.upcase}
+   puts languages 
+end
 
-
+def my_collection(students)
+  empty_array = []
+  students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
+    students.collect {|student| names}
+    yield(my_collection(students))
+    students.split(" ") [0]
+    puts students
+end
